afa59e747d6efd2bf03ff9eaedb6bcdb703dd058
Jami Kettunen <jami.kettunen@protonmail.com>
2023-01-05 23:15:31 +0200

    lomiri-ui-toolkit: ensure UITK_ICON_THEME=suru default
    Also install qt5-graphicaleffects since some of the used QML elements
    depend on this.

---
+++ srcpkgs/lomiri-ui-toolkit/files/lomiri-uitk-suru-icons.sh
@@ -0,0 +1,3 @@
+# set sane default for Lomiri UI toolkit icons which most apps rely on to exist
+# see also https://gitlab.com/ubports/development/core/lomiri-ui-toolkit/-/issues/21
+export UITK_ICON_THEME="suru"
